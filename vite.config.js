7fac1f2c6ba50fbc1812527ec1d4dd5ebccfd779
StevenHany0 <stevenhany513@gmail.com>
2026-03-23 18:39:30 +0200

    Add built Vite and bootstrap frontend assets

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // ensures output goes here
+        emptyOutDir: true,     // cleans folder before build
+    },
 });
