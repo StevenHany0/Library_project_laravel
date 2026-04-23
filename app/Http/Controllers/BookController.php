a4dc136f673b2e51f82d7f9b97585f62f9dd8a7a
StevenHany0 <stevenhany513@gmail.com>
2026-04-23 15:32:03 +0200

    Revert "book: robust image handling on update; preserve existing image when no new image uploaded; delete old image when replaced"
    This reverts commit cdfe20fb3071a4d9c6afab818fe160b66784e87b.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -109,20 +109,13 @@ class BookController extends Controller
         );
 
         $book = Book::find($request->id);
-        // Default to existing image
-        $imagePath = $book->image;
         if ($request->has('image')) {
             $file = $request->file('image');
             $extention = $file->getClientOriginalExtension();
             $filename = time() . '.' . $extention;
             $path = 'assets/files/';
             $file->move($path, $filename);
-            // Remove old image if it exists
-            if (File::exists($book->image)) {
-                File::delete($book->image);
-            }
-            // Update path to new image
-            $imagePath = $path . $filename;
+            
         }
 
         $book->update(
@@ -130,7 +123,7 @@ class BookController extends Controller
                 'name' => $request->name,
                 'description' => $request->description,
                 'price' => $request->price,
-                'image' => $imagePath,
+                'image' => $path . $filename,
                 'author_id' => $request->author_id,
                 'student_id' => $request->student_id
             ]
